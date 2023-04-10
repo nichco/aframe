7c22faa6e319b10db34009ae96eafb3b56c86c04
Nicholas Orndorff <norndorff@live.com>
2023-04-09 18:26:27 -0700

    setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='aframe',
+    version='0',
+    packages=[
+        'aframe',
+    ],
+    install_requires=[
+        'csdl',
+    ],
+)
