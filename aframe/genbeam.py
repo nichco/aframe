fc65cd3d03c98b45efba5b5514cb03ff7cd5cee2
Nicholas Orndorff <norndorff@live.com>
2023-04-09 22:01:42 -0700

    genbeam

---
+++ aframe/genbeam.py
@@ -0,0 +1,12 @@
+import numpy as np
+import csdl
+import python_csdl_backend
+
+
+# generates the required element dictionaries for a linear beam with 'n' nodes
+
+class GenBeam(csdl.Model):
+    def initialize(self):
+        pass
+    def define(self):
+        pass
