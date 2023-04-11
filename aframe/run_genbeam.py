b0c50b39548e42a8d567925d5f7c46b90d2b798e
nichco <norndorff@live.com>
2023-04-10 17:43:20 -0700

    generalize genbeam

--- aframe/run_genbeam.py
+++ aframe/run_genbeam.py
@@ -8,37 +8,74 @@ from aframe.group import Group
 
 class Run(csdl.Model):
     def initialize(self):
-        self.parameters.declare('options')
-        self.parameters.declare('bcond')
+        self.parameters.declare('options',default={})
+        self.parameters.declare('bcond',default={})
+        self.parameters.declare('beams',default={})
     def define(self):
         options = self.parameters['options']
         bcond = self.parameters['bcond']
+        beams = self.parameters['beams']
 
-        # generate the beam mesh:
-        beam_name = 'beam1'
+        
+
+        for beam_name in beams:
+            nodes = beams[beam_name]['nodes']
+            type = beams[beam_name]['type']
+            E = beams[beam_name]['E']
+            G = beams[beam_name]['G']
+            rho = beams[beam_name]['rho']
+
+            start = self.declare_variable(beam_name+'start',shape=(6),val=[0,0,0,0,0,0])
+            stop = self.declare_variable(beam_name+'stop',shape=(6),val=[10,0,0,0,0,0])
+            
+            for i in range(len(nodes) - 1):
+                element_name = beam_name + '_element_' + str(i)
+                options[element_name] = {}
+                # constant material properties:
+                options[element_name]['type'] = type
+                options[element_name]['E'], options[element_name]['G'], options[element_name]['rho'] = E, G, rho
+                # define the elemental start node and stop node from the node list:
+                options[element_name]['nodes'] = [nodes[i] , nodes[i+1]]
+                # compute the elemental start and stop node coordinates:
+                ds = (stop - start)/len(nodes)
+                node_a = start + ds*i
+                node_b = start + ds*(i + 1)
+                # register the outputs:
+                self.register_output(element_name+'node_a', node_a)
+                self.register_output(element_name+'node_b', node_b)
+
+        """
+        # generate the beam mesh(es):
+        beam_name = 'beam_1'
         start = self.declare_variable(beam_name+'start',shape=(6),val=[0,0,0,0,0,0])
         stop = self.declare_variable(beam_name+'stop',shape=(6),val=[10,0,0,0,0,0])
-        nodes = [0,1,2,3,4,5,6,7,8,9]
-        num_nodes = len(nodes)
+        nodes = beams[beam_name]['nodes']
+
+        type = beams[beam_name]['type']
+        E = beams[beam_name]['E']
+        G = beams[beam_name]['G']
+        rho = beams[beam_name]['rho']
         
-        for i in range(num_nodes - 1):
+        for i in range(len(nodes) - 1):
             element_name = 'element_' + str(i)
             options[element_name] = {}
             # constant material properties:
-            options[element_name]['type'] = 'tube'
-            options[element_name]['E'], options[element_name]['G'], options[element_name]['rho'] = 69E9, 26E9, 2700
+            options[element_name]['type'] = type
+            options[element_name]['E'], options[element_name]['G'], options[element_name]['rho'] = E, G, rho
             # define the elemental start node and stop node from the node list:
             options[element_name]['nodes'] = [nodes[i] , nodes[i+1]]
             # compute the elemental start and stop node coordinates:
-            ds = (stop - start)/num_nodes
+            ds = (stop - start)/len(nodes)
             node_a = start + ds*i
             node_b = start + ds*(i + 1)
             # register the outputs:
             self.register_output(element_name+'node_a', node_a)
             self.register_output(element_name+'node_b', node_b)
-
-
-        # generate the loads vector:
+        """
+        
+        # generate the loads vector(s) (for each beam if there are multiple beams):
+        #beam_name = 'beam1'
+        #loads = self.declare_variable(beam_name+'loads',shape=(6,len(nodes)))
 
 
         
@@ -56,6 +93,7 @@ class Run(csdl.Model):
         # solve the beam group:
         self.add(Group(options=options,bcond=bcond), name='Group')
         
+        
 
 
 
@@ -64,20 +102,37 @@ class Run(csdl.Model):
 
 if __name__ == '__main__':
 
-    options = {}
+    options, bcond, beams = {}, {}, {}
 
-    bcond = {}
+    bcond['root1'] = {}
+    bcond['root1']['node'] = 0
+    bcond['root1']['fdim'] = [1,1,1,1,1,1] # [x, y, z, phi, theta, psi]: a 1 indicates the corresponding dof is fixed
 
-    name = 'root'
-    bcond[name] = {}
-    bcond[name]['node'] = 0
-    bcond[name]['fdim'] = [1,1,1,1,1,1] # x, y, z, phi, theta, psi: a 1 indicates the corresponding dof is fixed
-
-
-
+    bcond['root2'] = {}
+    bcond['root2']['node'] = 10
+    bcond['root2']['fdim'] = [1,1,1,1,1,1] # [x, y, z, phi, theta, psi]: a 1 indicates the corresponding dof is fixed
 
 
-    sim = python_csdl_backend.Simulator(Run(options=options,bcond=bcond))
+    name = 'beam_1'
+    beams[name] = {}
+    beams[name]['nodes'] = [0,1,2,3,4,5,6,7,8,9]
+    beams[name]['type'] = 'tube'
+    beams[name]['E'] = 69E9
+    beams[name]['G'] = 26E9
+    beams[name]['rho'] = 2700
+
+    name = 'beam_2'
+    beams[name] = {}
+    beams[name]['nodes'] = [10,11,12,13,14,15,16,17,18,19]
+    beams[name]['type'] = 'tube'
+    beams[name]['E'] = 69E9
+    beams[name]['G'] = 26E9
+    beams[name]['rho'] = 2700
+
+
+
+
+    sim = python_csdl_backend.Simulator(Run(options=options,bcond=bcond,beams=beams))
     sim.run()
 
     
