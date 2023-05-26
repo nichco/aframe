938e28db03a8598f85dbb824494acda4d11ed57b
nichco <norndorff@live.com>
2023-05-26 14:32:38 -0700

    test

---
+++ test.py
@@ -0,0 +1,24 @@
+
+
+
+
+
+
+class Aframe:
+    def __init__(self):
+        pass
+
+    def add_beam(self, name, mesh, index):
+        pass
+
+    def add_load(self):
+        pass
+
+    def add_joint(self):
+        pass
+
+    def add_bc(self):
+        pass
+
+    def solve(self):
+        pass
